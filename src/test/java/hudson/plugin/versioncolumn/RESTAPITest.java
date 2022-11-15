020d2748e597256fbecc700b3b9954f319e7c30d
Mark Waite <mark.earl.waite@gmail.com>
2022-11-15 10:32:52 -0700

    Test XML result from computer/api/xml end point
    An unsuccessful attempt to show the failure reported as
    https://issues.jenkins.io/browse/JENKINS-70074 .  It is unclear to me why
    interactive testing easily duplicated the failure, while this automated
    test does not show the failure.

---
+++ src/test/java/hudson/plugin/versioncolumn/RESTAPITest.java
@@ -0,0 +1,55 @@
+package hudson.plugin.versioncolumn;
+
+import com.gargoylesoftware.htmlunit.xml.XmlPage;
+import hudson.model.User;
+import hudson.security.HudsonPrivateSecurityRealm;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.JenkinsRule.WebClient;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.xml.HasXPath.hasXPath;
+
+public class RESTAPITest {
+    @Rule
+    public JenkinsRule rule = new JenkinsRule();
+
+    private final String USER_NAME = "user-for-RESTAPITest";
+
+    private WebClient webClient = null;
+
+    @Before
+    public void setup() throws Exception {
+        final String PASSWORD = "password-for-RESTAPITest";
+        HudsonPrivateSecurityRealm securityRealm = new HudsonPrivateSecurityRealm(false, false, null);
+        rule.jenkins.setSecurityRealm(securityRealm);
+
+        User user = securityRealm.createAccount(USER_NAME, PASSWORD);
+        user.setFullName("User for REST API test");
+        user.save();
+
+        webClient = rule.createWebClient();
+        webClient.login(USER_NAME, PASSWORD);
+   }
+
+    @Test
+    public void securedAPILoginTest() throws Exception {
+        // Check that authnenticated access to the API is allowed
+        XmlPage w1 = (XmlPage) webClient.goTo("whoAmI/api/xml", "application/xml");
+        assertThat(w1, hasXPath("//name", is(USER_NAME)));
+    }
+
+    @Issue("JENKINS-70074")
+    @Test
+    public void securedAPITest() throws Exception {
+        // Interactive testing failed while this automated test did
+        // not fail.  Unclear why they differ. Including this in the
+        // test suite because it may prevent future issues.
+        XmlPage computerApi = (XmlPage) webClient.goTo("computer/api/xml", "application/xml");
+        assertThat(computerApi, hasXPath("//name", is("built-in")));
+    }
+}
